8880cb32ba19b9dbd390e3ba03d5b63f9a4b6a59
SusInMotion <steinman.tutoring@gmail.com>
2016-07-25 16:26:36 -0400

    not sure...

--- esummary.py
+++ esummary.py
@@ -1,103 +1,114 @@
 import requests
 import lxml
-import xml.etree.ElementTree as et
+from xml.etree import ElementTree as et
 from datetime import date, timedelta as td
 import logging
+import time
+import psycopg2
+from secret import dict_cur
+from psycohandler import *
+import sys 
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
 
 #create a function that gets UID from date http://www.ncbi.nlm.nih.gov/pubmed?term=1997%2F10%2F06/ <= thjat's a sample query
 def main(start_month, start_day, start_year, no_of_days_to_check):
-
+    start_time=time.time()
     start_date = date(start_year,start_month,start_day)
-
     for i in range(no_of_days_to_check + 1):
-        print (start_date+td(days=i)).strftime("%Y/%m/%d")
-        getUIDsFromDate( (start_date+td(days=i)).strftime("%Y/%m/%d"))
+        uids=getUIDsFromDate( (start_date+td(days=i)).strftime("%Y/%m/%d"))
+        print len(uids)
+        for uID in uids:
+            tree= queryEsummaryByUID(uID)
+            print getAllFromEsumXML(tree, uID)
+
+    print time.time()-start_time
 
 
-
-def getUIDsFromDate(date):
+def queryEsearchByDate(date):
     query="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=10000&term={}[pdat]".format(date)
     print query
     get_query=requests.get(query)
-    list_ids=[]
     try:
-        content=get_query.content
+        content=get_query.content        
+        tree= et.fromstring(content)
+        return tree
 
-        with open('temp.xml','wb') as f:
-            f.write(content)
-        e= et.parse('temp.xml').getroot()
-        
-        for id in e.find('IdList').getchildren():
-            list_ids.append(id.text)
-        print len(list_ids)
-        return list_ids
     except Exception as e:
         logger.error("No content found for %s, %s. Exception %s thrown", date, query, e)
+        
+def getUIDsFromEsearchXML(tree):   
+    list_ids=[]
+    try:    
+        for id in tree.find('IdList').getchildren():
+            list_ids.append(id.text)
+
+    except Exception as e:
+        logger.error("Cannot find id list for %s. Exception %s thrown", date, e)
+
+    return list_ids
+
+def getUIDsFromDate(date):
+    tree=queryEsearchByDate(date)
+    return getUIDsFromEsearchXML(tree)
 
 
-
-def query(uID):
+def queryEsummaryByUID(uID):
     query="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id={}&version=2.0".format(uID)
     get_query=requests.get(query)
-    content=get_query.content
-    with open('temp.xml','wb') as f:
-        f.write(content)
-    e= et.parse('temp.xml').getroot()
-    return e
+    try:
+        content=get_query.content
+        tree= et.fromstring(content)
+        return tree
 
-def getcitationsfromquery(results):
-    citations=results.find('DocumentSummarySet').find('DocumentSummary').find('PmcRefCount').text
-    return citations
+    except Exception as e:
+        logger.error("No content found for %s, %s. Exception %s thrown", uID, query, e)
+        
 
-def getcitations(uID):
-    x=query(uID)
-    return int (getcitationsfromquery(x))
-
-
-def extractAuthors(authors):
-    values=[]
-    columns = []
+def extractAuthors(authors, uid):
     for author in authors:
-        values.append(author[0].text)
-        columns.append(author[0].tag)
-        print authors.tag,tuple(columns),tuple(values)
-        check_insert_select('id','authors',tuple(columns),tuple(values))
-    return authors.tag,tuple(columns),tuple(values)
+        print author.find("Name").text
+        authorID=check_insert_select('id','authors',("Name",),(author.find("Name").text,))[0][0]
+        check_insert("id", "Authors_Papers", ("AuthorId", "PaperId"), (authorID, uid))
 
 
 def extractArticleIds(articleIds):
+    columns=[]
     values = []
     for articleId in articleIds:
-        subvalues = []
-        columns = []
-        for tag in articleId:
-            subvalues.append(tag.text)
-            columns.append(tag.tag)
-        values.append([articleIds.tag,tuple(columns),tuple(subvalues)])
-    print values
-    check_insert('id','papers',tuple(columns),tuple(subvalues))
-    return values
+            columns.append(articleId.find("IdType").text.replace("-",""))
+            values.append(articleId.find("Value").text)
+    return columns, values
 
 
-def getallfromquery(root):
+def getAllFromEsumXML(tree, uid):
     list_tags=['Authors','ArticleIds']
-    doc_summary = root.find('DocumentSummarySet').find('DocumentSummary')
+    doc_summary = tree.find('DocumentSummarySet').find('DocumentSummary')
     rec={}
+    columns=[]
+    values=[]
     for child in doc_summary:
         if child.tag == 'Authors':
-            extractAuthors(child)
+            authorIDs=extractAuthors(child, uid)
         elif child.tag == 'ArticleIds':
-            extractArticleIds(child)
+            artcolumns, artvalues=extractArticleIds(child)
+            for i in range(len(artcolumns)): 
+                rec[artcolumns[i]]=artvalues[i]
+                columns.append(artcolumns[i])
+                values.append(artvalues[i])
         else:
             rec[child.tag] = child.text
+            columns.append(child.tag)
+            values.append(child.text)
+    logging.debug(columns, "columns")
+    logging.debug(values, "values")
+    check_insert("uid","papers", tuple(columns),tuple(values))
     return rec
-
-
-def getall(uID):
-    return getallfromquery(query(uID))
 
 
 
@@ -108,7 +119,7 @@ def getall(uID):
 
 
 
-
+'''
 def getotherthingsfromquery(results):
     otherthings=results.find('DocumentSummarySet').find('DocumentSummary').find('Authors').text
     return otherthings
@@ -119,11 +130,23 @@ def getothers(uID):
     a=query(uID)
     return str (getotherthingsfromquery(a))
 
+def getcitationsfromXML(tree):
+    try:
+        citations=tree.find('DocumentSummarySet').find('DocumentSummary').find('PmcRefCount').text
+        return int(citations)
+
+    except Exception as e:
+        logger.error("Cannot find citations. Exception %s thrown", e)
+        return None
+
+def getCitationsFromUID(uID):
+    tree= queryEsummaryByUID(uID)
+    return getcitationsfromXML(tree)
+
+'''
 
 
-
-
-def finding_abstracts(uID):
+def getAbstractFromUID(uID):
     a=requests.get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={}'.format(uID))
     if str(a)=='<Response [200]>':
         content=a.content
@@ -153,11 +176,4 @@ def getarticleinfo(minuID,maxuID):
         abstract=finding_abstracts(i)
         citations=getcitations(i)
         print i,abstract,citations
-
-
-def check_insert(values_wanted, database_name, colnames=(), values=()):
-        results = select(values_wanted, database_name, colnames, values)
-        if results == []:
-            insert(database_name, colnames, values)
-        return
 
